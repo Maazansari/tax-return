4c100e97cb06552c9e2d2f7ab929e8a5781bb6af
Maaz Ansari <k142203@nu.edu.pk>
2020-08-05 01:42:58 +0500

    add icon for submitted forms

--- src/tax-form.js
+++ src/tax-form.js
@@ -2,25 +2,34 @@ import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 
 import TextField from "./Text-field";
+
+import tickIcon from "./images/tick.svg";
 class TaxForm extends Component {
-  state = { formValues: {} };
+  state = { formValues: {}, submitted: false };
   setFieldValue = (name, value) => {
     this.setState((prevState) => ({
       formValues: { ...prevState.formValues, [name]: value },
+      submitted: false,
     }));
   };
   onFormSubmit = () => {
     const { id, setFormState } = this.props;
     const { formValues } = this.state;
+    this.setState({ submitted: true });
     setFormState(id, formValues);
   };
   render() {
     const { formFields, deleteForm, id } = this.props;
-    const { formValues } = this.state;
+    const { formValues, submitted } = this.state;
     return (
       <div>
         <form className="form">
-          {id && <h6>{`ID: #${id}`}</h6>}
+          {id && (
+            <h6>
+              {`ID: #${id}`} {submitted && <img src={tickIcon} width={15} />}
+            </h6>
+          )}
+
           {formFields.map((field) => (
             <TextField
               {...field}
